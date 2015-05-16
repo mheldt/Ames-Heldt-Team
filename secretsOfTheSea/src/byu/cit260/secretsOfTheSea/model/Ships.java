32d0f6970447d22cfe2b663304900a04a74fae57
mheldt <mrkhldt@gmail.com>
2015-05-16 02:02:35 -0500

    5/15/2015 - added Ships

---
+++ secretsOfTheSea/src/byu/cit260/secretsOfTheSea/model/Ships.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byu.cit260.secretsOfTheSea.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author MarkH
+ */
+public class Ships implements Serializable{
+    
+    // class instance variables
+    private String shipType;
+    private String inventoryComplete;
+
+    public Ships() {
+    }
+
+    
+    public String getShipType() {
+        return shipType;
+    }
+
+    public void setShipType(String shipType) {
+        this.shipType = shipType;
+    }
+
+    public String getInventoryComplete() {
+        return inventoryComplete;
+    }
+
+    public void setInventoryComplete(String inventoryComplete) {
+        this.inventoryComplete = inventoryComplete;
+    }
+
+    @Override
+    public String toString() {
+        return "Ships{" + "shipType=" + shipType + ", inventoryComplete=" + inventoryComplete + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 71 * hash + Objects.hashCode(this.shipType);
+        hash = 71 * hash + Objects.hashCode(this.inventoryComplete);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Ships other = (Ships) obj;
+        if (!Objects.equals(this.shipType, other.shipType)) {
+            return false;
+        }
+        if (!Objects.equals(this.inventoryComplete, other.inventoryComplete)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
